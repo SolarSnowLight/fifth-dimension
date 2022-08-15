4097238950b32254d8776f0b312d250d3409e432
DanSoW <67583913+DanSoW@users.noreply.github.com>
2022-08-15 15:24:06 +0800

    feat(web-landing): update landing page for payment

---
+++ web-landing/src/constants/api/payment.api.js
@@ -0,0 +1,7 @@
+
+const PaymentApiConstants = {
+    payment_success: '/api/payment/success', //'https://kristinakaruna.ru',
+    payment_failed: '/api/payment/failed'
+};
+
+export default PaymentApiConstants;
--- web-landing/src/containers/Payment/Payment.jsx
+++ web-landing/src/containers/Payment/Payment.jsx
@@ -1,14 +1,12 @@
 import { useEffect } from 'react';
 import { useSearchParams } from 'react-router-dom';
+import MainApiConstants from '../../constants/api/main.api';
+import PaymentApiConstants from '../../constants/api/payment.api';
 import styles from './Payment.module.css';
 
 const Payment = () => {
     const [searchParams, setSearchParams] = useSearchParams();
     const confirmationToken = searchParams.get("confirmation_token");
-    const userId = searchParams.get("user_id");
-    const paymentKey = searchParams.get("payment_key");
-    const successKey = searchParams.get("success_key");
-    const tariffId = searchParams.get("tariffId");
 
     // Инициализация виджета от ЮKassa. Все параметры обязательные.
     const checkout = new window.YooMoneyCheckoutWidget({
@@ -19,10 +17,26 @@ const Payment = () => {
         }
     });
 
-
     /* Обработка ситуации успешного платежа пользователем */
-    const successHandler = () => {
+    const successHandler = async() => {
+        const response = await fetch(
+            (MainApiConstants.main_server + PaymentApiConstants.payment_success),
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    confirmation_token: confirmationToken
+                })
+            }
+        );
 
+        if(response.ok){
+            // redirect to ok
+        }
+
+        // redirect to non ok
     };
 
     /* Обработка ситуации завершения платежа пользователем */
